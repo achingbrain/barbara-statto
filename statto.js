ea8f182bb83cad0048ea171f291525c4946fbdc3
achingbrain <alex@achingbrain.net>
2013-11-09 13:06:47 +0000

    added dependencies, changed name

--- statto.js
+++ statto.js
@@ -1,9 +1,10 @@
-var common = require("../brewbot-common"),
-	nconf = require("nconf"),
+var nconf = require("nconf"),
 	Container = require("wantsit").Container,
 	bonvoyage = require("bonvoyage"),
-	Nano = require('nano'),
-	Columbo = require("columbo");
+	Nano = require("nano"),
+	Columbo = require("columbo"),
+	Hapi = require("hapi"),
+	LOG = require("winston");
 
 // set up arguments
 nconf.argv().env().file("config.json");
@@ -16,31 +17,43 @@ var connection = new Nano("http://" + nconf.get("couchdb:host") + ":" + nconf.ge
 container.register("connection", connection);
 
 // create collections
-connection.db.create("brews");
+connection.db.create("barbara_brews");
 container.register("brewsDb", connection.use("barbara_brews"));
 
 // repositories
 container.createAndRegister("brewRepository", require("./repositories/BrewRepository"));
 
 // create a REST api
-container.createAndRegister("resourceDiscoverer", Columbo, {
+container.createAndRegister("columbo", Columbo, {
+	resourceDirectory: nconf.get("rest:resources"),
 	resourceCreator: function(resource, name) {
-		return container.createAndRegister(name, resource);
+		return container.createAndRegister(name + "Resource", resource);
 	}
 });
-container.createAndRegister("restServer", common.rest.RESTServer);
 
 var bonvoyageClient = new bonvoyage.Client({
 	serviceType: nconf.get("registry:name")
 });
 bonvoyageClient.find(function(error, seaport) {
+	if(error) {
+		LOG.error("Could not find SeaPort", error);
+
+		return;
+	}
+
 	container.register("seaport", seaport);
 });
 bonvoyageClient.register({
 	role: nconf.get("rest:name"),
 	version: nconf.get("rest:version"),
 	createService: function(port) {
-		var restServer = container.find("restServer");
-		restServer.start(port);
+		var columbo = container.find("columbo");
+		var server = Hapi.createServer("0.0.0.0", port, {
+			cors: true
+		});
+		server.addRoutes(columbo.discover());
+		server.start();
+
+		LOG.info("RESTServer", "Running at", "http://localhost:" + port);
 	}
 });
